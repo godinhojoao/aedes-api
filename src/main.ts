9db5bed6c302f35f91b0f4120bacd1121f20d4be
João Godinho Jardim <godinhojoao@MacBook-Air-de-Joao.local>
2023-06-30 22:00:45 -0300

    feat: enabling cors for localhost aedes-web

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: false,
+  });
   await app.listen(3000);
 }
 bootstrap();
